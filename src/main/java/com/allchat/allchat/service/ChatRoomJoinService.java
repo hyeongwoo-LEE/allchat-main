f77260afa7e982f5810a4a0e1783ea100b69a16f
hyeongwoo-LEE <lhw970804@naver.com>
2021-10-31 17:49:04 +0900

    방참여 기능

--- src/main/java/com/allchat/allchat/service/ChatRoomJoinService.java
+++ src/main/java/com/allchat/allchat/service/ChatRoomJoinService.java
@@ -1,4 +1,32 @@
 package com.allchat.allchat.service;
 
+import com.allchat.allchat.domain.chatRoom.ChatRoom;
+import com.allchat.allchat.domain.chatRoomJoin.ChatRoomJoin;
+import com.allchat.allchat.domain.chatRoomJoin.ChatRoomJoinRepository;
+import com.allchat.allchat.domain.chatRoomJoin.RoleType;
+import com.allchat.allchat.domain.user.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@RequiredArgsConstructor
+@Service
 public class ChatRoomJoinService {
+
+    private final ChatRoomJoinRepository chatRoomJoinRepository;
+
+    public ChatRoomJoin join(Long chatRoomId, Long principalId){
+
+        ChatRoomJoin chatRoomJoin = ChatRoomJoin.builder()
+                .chatRoom(ChatRoom.builder().chatRoomId(chatRoomId).build())
+                .user(User.builder().userId(principalId).build())
+                .role(RoleType.GUEST)
+                .build();
+
+        chatRoomJoinRepository.save(chatRoomJoin);
+
+        return chatRoomJoin;
+    }
+
 }
