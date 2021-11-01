305cd2c9f8a9631d344981865398c9f99ded6439
hyeongwoo-LEE <lhw970804@naver.com>
2021-11-01 20:12:00 +0900

    1. 채팅방 생성 2. 채팅방 참여 3. 채팅방나가기 controller 생성

--- src/main/java/com/allchat/allchat/controller/ChatRoomController.java
+++ src/main/java/com/allchat/allchat/controller/ChatRoomController.java
@@ -1,18 +1,40 @@
 package com.allchat.allchat.controller;
 
+import com.allchat.allchat.config.auth.PrincipalDetails;
 import com.allchat.allchat.dto.CMRespDTO;
+import com.allchat.allchat.dto.chatRoom.ChatRoomDTO;
+import com.allchat.allchat.service.ChatRoomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Required;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/chatrooms")
+@RequiredArgsConstructor
 @RestController
 public class ChatRoomController {
 
+    private final ChatRoomService chatRoomService;
+
+    /**
+     * 채팅방 목록
+     */
+    @GetMapping
+    public ResponseEntity<?> chatRoomList(){
+        return null;
+    }
+
+    /**
+     * 채팅방 생성
+     */
     @PostMapping
-    public ResponseEntity<?> createChatRoom(){
+    public ResponseEntity<?> createChatRoom(@RequestBody ChatRoomDTO chatRoomDTO,
+                                            @AuthenticationPrincipal PrincipalDetails principalDetails){
+
+        chatRoomService.create(chatRoomDTO.getTitle(), principalDetails.getUser().getUserId());
+
         return new ResponseEntity<>(new CMRespDTO<>(1, "채팅방 생성 성공", null), HttpStatus.CREATED);
     }
 }
--- src/main/java/com/allchat/allchat/controller/ChatRoomJoinController.java
+++ src/main/java/com/allchat/allchat/controller/ChatRoomJoinController.java
@@ -1,4 +1,60 @@
 package com.allchat.allchat.controller;
 
+
+import com.allchat.allchat.config.auth.PrincipalDetails;
+import com.allchat.allchat.dto.CMRespDTO;
+import com.allchat.allchat.dto.chatRoomJoin.ChatRoomJoinDTO;
+import com.allchat.allchat.service.ChatRoomJoinService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
 public class ChatRoomJoinController {
+
+    private final ChatRoomJoinService chatRoomJoinService;
+
+    /**
+     * 채팅방 참여목록
+     */
+    @GetMapping("/chatrooms/{chatRoomId}/joins")
+    public ResponseEntity<?> chatRoomJoinList(){
+        return null;
+    }
+
+    /**
+     * 채팅방 참여
+     */
+    @PostMapping("/joins")
+    public ResponseEntity<?> join(@RequestBody ChatRoomJoinDTO chatRoomJoinDTO,
+                                  @AuthenticationPrincipal PrincipalDetails principalDetails){
+
+        if(!principalDetails.getUser().getUserId().equals(chatRoomJoinDTO.getUserId())){
+            throw new IllegalStateException("참여 권한이 없습니다.");
+        }
+
+        chatRoomJoinService.join(chatRoomJoinDTO.getChatRoomId(), chatRoomJoinDTO.getUserId());
+
+        return new ResponseEntity<>(new CMRespDTO<>(1, "채팅방 참여 성공", null), HttpStatus.CREATED);
+    }
+
+    /**
+     * 채팅방 나가기
+     */
+    @DeleteMapping("/joins/{chatRoomJoinId}")
+    public ResponseEntity<?> getOut(@RequestBody ChatRoomJoinDTO chatRoomJoinDTO,
+                                    @AuthenticationPrincipal PrincipalDetails principalDetails){
+
+        if(!principalDetails.getUser().getUserId().equals(chatRoomJoinDTO.getUserId())){
+            throw new IllegalStateException("채팅방 나가기 권한이 없습니다.");
+        }
+
+        chatRoomJoinService.remove(chatRoomJoinDTO.getChatRoomId(), chatRoomJoinDTO.getUserId());
+
+        return new ResponseEntity<>(new CMRespDTO<>(1, "채팅방 나가기 성공", null), HttpStatus.OK);
+    }
+
 }
